be570372933493616909d00894798e6639a8e7a6
Chris <chrl@spreadshirt.net>
2016-04-08 13:40:18 +0200

    Removed unused files.

--- src/org/netbeans/modules/nbtasks/nodes/childnodes/INbTasksChildNodeFactory.java
+++
@@ -1,9 +0,0 @@
-package org.netbeans.modules.nbtasks.nodes.childnodes;
-
-/**
- *
- * @author chrl
- */
-public interface INbTasksChildNodeFactory {
-    public INbTasksChildNodeFactory getChildNodeFactory();
-}
