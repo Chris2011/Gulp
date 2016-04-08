a06563b3de236e93da9bdf94a725cf6d2f3079d5
Chris <chrl@spreadshirt.net>
2016-04-08 11:42:48 +0200

    Added method to get childNodeFactories.

--- src/org/netbeans/modules/nbtasks/nodes/childnodes/INbTasksNode.java
+++ src/org/netbeans/modules/nbtasks/nodes/childnodes/INbTasksNode.java
@@ -1,13 +1,15 @@
 package org.netbeans.modules.nbtasks.nodes.childnodes;
 
 import java.awt.Image;
+import org.openide.nodes.ChildFactory;
 
 /**
  *
  * @author chrl
  */
-public interface INbTasksNode {
+public interface INbTasksNode<T> {
     public String getDisplayName();
     public Image getIcon(int type);
     public Image getOpenedIcon(int type);
+    public ChildFactory<T> getChildNodeFactory();
 }
