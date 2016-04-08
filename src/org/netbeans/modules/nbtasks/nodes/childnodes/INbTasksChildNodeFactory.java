4480e7cc1aa92cd1b67d31e8113962fc798dfea3
Chris <chrl@spreadshirt.net>
2016-04-08 11:42:32 +0200

    Added interface for common childNodeFactories to show it.

---
+++ src/org/netbeans/modules/nbtasks/nodes/childnodes/INbTasksChildNodeFactory.java
@@ -0,0 +1,9 @@
+package org.netbeans.modules.nbtasks.nodes.childnodes;
+
+/**
+ *
+ * @author chrl
+ */
+public interface INbTasksChildNodeFactory {
+    public INbTasksChildNodeFactory getChildNodeFactory();
+}
